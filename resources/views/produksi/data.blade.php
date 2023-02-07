85f6323830016bacbe79852a4c2082d6798a2fca
maryam123-bum <124107075+maryam123-bum@users.noreply.github.com>
2023-02-07 23:13:01 +0700

    Update data.blade.php

--- resources/views/produksi/data.blade.php
+++ resources/views/produksi/data.blade.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Produksi</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-10">
-                <h1>Data <?php echo $judul; ?></h1>
-            </div>
-            <div class="col-2">
-                <button class="btn btn-success">
-                    Tambah <?php echo $judul; ?>
-                </button>
-            </div>
-        </div>
+@extends('layouts/main')
+@section('title')
+    <h2>Data {{ $judul }}</h2> 
+@endsection
+@section('container')
+    <div class="col-2">
+        <button class="btn btn-success">
+            Tambah <?php echo $judul; ?>
+        </button>
+    </div>
         <div class="row">
             <div class="col">
                 <table class="table table-bordered">
@@ -50,6 +39,6 @@
             </div>
         </div>
     </div>
-</body>
+@endsection
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </html>
