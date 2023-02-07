ae34398be1640de574fbb9081b278b6532ef6a88
maryam123-bum <124107075+maryam123-bum@users.noreply.github.com>
2023-02-07 23:11:58 +0700

    Create dashboard.blade.php

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts/main')
+@section('title')
+    <h2>DASHBOARD</h2> 
+@endsection
+@section('container')
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            1 of 2
+          </div>
+          <div class="col">
+            2 of 2
+          </div>
+        </div>
+    </div>
+@endsection
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</html>
