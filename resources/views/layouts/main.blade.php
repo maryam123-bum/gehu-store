89f62d4f3baa5a1ab3899042a997f0d5979fc2a4
Aji M. Iqbal Fadhilah <57173064+Jieeevest@users.noreply.github.com>
2023-02-19 22:18:52 +0700

    sad

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -249,5 +249,6 @@
       <script src="{!! asset('js/bootstrap2.bundle.min.js') !!}"></script>
       <script src="{!! asset('js/popper.min.js') !!}"></script>
       <script src="{!! asset('js/bootstrap.min.js') !!}" ></script>
+      <script src="{!! asset('js/sweetalert.min.js') !!}"></script>
       @yield('script')
     </html>
